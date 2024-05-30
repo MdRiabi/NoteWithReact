03c67d3da7693c204c65df1741f45d5f9484ec12
MdRiabi <mhdriabi2013@gmail.com>
2024-05-30 23:12:40 +0200

    "Added Video component CSS and JSX files with styles and markup for video section"

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,9 +1,19 @@
-
+import React from "react"
+import "./Video.css"
+import videoImg from "../../assets/video.png";
+import TLgradient from "../../assets/vid-top-left-gradient.png"
+import BRgradient from "../../assets/vid-bottom-right-gradient.png"
 
 
 const Video = () => {
   return (
-    <div>Video</div>
+    <section className="video">
+      <img src={videoImg} alt="" className="video__image" />
+      <button className="video__play"></button>
+      <div className="video__overlay"></div>
+      <img src={TLgradient} alt="" className="video-gradient video__tl-gradient" />
+      <img src={BRgradient} alt="" className="video-gradient video__br-gradient" />
+    </section>
   )
 }
 
