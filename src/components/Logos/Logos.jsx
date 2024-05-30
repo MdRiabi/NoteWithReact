c81e3b817e54d0d1bf4db609dbfc2335bf2721e1
MdRiabi <mhdriabi2013@gmail.com>
2024-05-30 20:24:33 +0200

    "Added CSS styles and JSX code for Logos component and using logos constant from utils folder to show the logos"

--- src/components/Logos/Logos.jsx
+++ src/components/Logos/Logos.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { logos } from '../../utils/constants'
+import './Logos.css'
 
 const Logos = () => {
   return (
-    <div>Logos</div>
+    <section className="logos">
+      <div className="logos__slide">
+        {
+          logos.map((logo, index) => (
+            <img src={logo} alt="logo" key={index} className="logo" />
+          ))
+        }
+         {
+          logos.map((logo, index) => (
+            <img src={logo} alt="logo" key={index} className="logo" />
+          ))
+        }
+      </div>
+      <div className="logos__overlay logos__overlay_left"></div>
+      <div className="logos__overlay logos__overlay_right"></div>
+    </section>
   )
 }
 
