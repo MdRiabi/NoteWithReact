7e38d4d8a8b4f55116a45ac51b70e4cfc8bd4c98
MdRiabi <mhdriabi2013@gmail.com>
2024-05-31 02:15:11 +0200

    "Updated App component with new Testimonial section, added FAQ component with CSS and JSX changes"

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./FAQ.css";
 
-const FAQ = () => {
+const FAQ = ({ children }) => {
   return (
-    <div>FAQ</div>
-  )
-}
+    <section className="faq">
+      <div className="faq__heading-section">
+        <h2 className="h2 faq__heading">Frequently Asked Questions</h2>
+        <p className="text-reg faq__description">
+          We've compiled a list of the most frequently asked questions about
+          SmartNotes to help you get the information you need. If you have any
+          other questions, feel free to reach out to our support team.
+        </p>
+      </div>
+      {children}
+    </section>
+  );
+};
 
-export default FAQ
+export default FAQ;
